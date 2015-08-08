b4f14fe5398c7f240da4f349ac239643be500756
Keith Trnka <keith.trnka@gmail.com>
2015-08-08 13:51:56 -0700

    Split out Riot API

--- src/view_match.py
+++ src/view_match.py
@@ -2,13 +2,11 @@ from __future__ import unicode_literals
 import ConfigParser
 import collections
 import logging
-import pprint
 import sys
 import argparse
 import math
-import requests
-import urlparse
-import time
+
+from src.riot_api import RiotService
 
 
 def parse_args():
@@ -16,65 +14,6 @@ def parse_args():
     parser.add_argument("config", help="Config file")
     parser.add_argument("match_id", help="ID of the match to use")
     return parser.parse_args()
-
-
-class RiotService(object):
-    def __init__(self, base_url, static_base_url, api_key):
-        self.base_url = base_url
-        self.static_base_url = static_base_url
-        self.api_key = api_key
-        self.params = {"api_key": self.api_key}
-        self.champions = None
-
-        self.most_recent_request = None
-        self.delay_seconds = 1.2
-        self.logger = logging.getLogger("RiotService")
-
-    def request(self, endpoint):
-        self.throttle()
-        response = requests.get(urlparse.urljoin(self.base_url, endpoint), params=self.params)
-        response.raise_for_status()
-        data = response.json()
-        return data
-
-    def get_match(self, match_id):
-        return self.request("v2.2/match/{}".format(match_id))
-
-    def get_champion_info(self, champion_id):
-        if not self.champions:
-            self.champions = self.get_champions()
-
-        return self.champions[champion_id]
-
-    def request_static(self, endpoint):
-        self.throttle()
-        response = requests.get(urlparse.urljoin(self.static_base_url, endpoint), params=self.params)
-        response.raise_for_status()
-        data = response.json()
-        return data
-
-    def get_champions(self):
-        data = self.request_static("v1.2/champion")
-        data = data["data"]
-
-        return {value["id"]: value for value in data.itervalues()}
-
-    def throttle(self):
-        if self.most_recent_request and time.clock() - self.most_recent_request < self.delay_seconds:
-            self.logger.debug("Sleeping for %.1f seconds", self.delay_seconds)
-            time.sleep(self.delay_seconds)
-        self.most_recent_request = time.clock()
-
-    def get_team_name(self, team_id):
-        if team_id == 100:
-            return "Blue"
-        elif team_id == 200:
-            return "Red"
-        else:
-            return "Unknown, " + team_id
-
-    def get_summoner_ranked_stats(self, summoner_id):
-        return self.request("v1.3/stats/by-summoner/{summonerId}/ranked".format(summonerId=summoner_id))
 
 
 def main():
