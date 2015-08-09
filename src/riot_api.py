069be0cfdd1723ebbb49f2dda705029307f6bbbf
Keith Trnka <keith.trnka@gmail.com>
2015-08-08 18:28:33 -0700

    Slower rate

--- src/riot_api.py
+++ src/riot_api.py
@@ -18,7 +18,7 @@ class RiotService(object):
         self.champions = None
 
         self.most_recent_request = None
-        self.delay_seconds = 1.2
+        self.delay_seconds = 1.3
         self.logger = logging.getLogger("RiotService")
 
     @staticmethod
