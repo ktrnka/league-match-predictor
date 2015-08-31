1b5d7f94bfa93d28a3022b07479e85b1f4f866c9
Keith Trnka <keith.trnka@gmail.com>
2015-08-31 13:18:11 -0700

    Cleaning up the experimental code

--- src/exploration/train_test.py
+++ src/exploration/train_test.py
@@ -73,8 +73,9 @@ def convert_to_indicators(data, column, drop=True, min_values=1):
         data.drop(column, axis=1, inplace=True)
 
 
-def print_feature_importances(columns, tuned_classifier):
-    paired_features = zip(columns, tuned_classifier.best_estimator_.feature_importances_)
+def print_feature_importances(columns, classifier):
+    """Show feature importances for a classifier that supports them like random forest or gradient boosting"""
+    paired_features = zip(columns, classifier.feature_importances_)
     print "Feature importances"
     for feature_name, importance in sorted(paired_features, key=itemgetter(1), reverse=True):
         print "\t{:20s}: {}".format(feature_name, importance)
@@ -105,7 +106,7 @@ def random_forest(X, y, data, split_iterator):
 
     print "Random forest"
     print_tuning_scores(grid_search)
-    print_feature_importances(data.drop("IsBlueWinner", axis=1).columns, grid_search)
+    print_feature_importances(data.drop("IsBlueWinner", axis=1).columns, grid_search.best_estimator_)
 
 def decision_tree(X, y, data, split_iterator):
     tree = sklearn.tree.DecisionTreeClassifier()
@@ -145,10 +146,7 @@ def logistic_regression(X, y, data, split_iterator, pca=False):
     print "Logistic regression"
     print_tuning_scores(grid_search)
 
-    # The feature weights really aren't interpretable without also having the scale of the feature
-    # print_logistic_regression_feature_importances(data.drop("IsBlueWinner", axis=1).columns, grid_search.best_estimator_)
-
-    # with feature scaling
+    # with feature scaling, mostly useful to get reasonable feature importances
     X_scaled = sklearn.preprocessing.scale(X)
     grid_search = sklearn.grid_search.GridSearchCV(logistic, hyperparameter_space, n_jobs=N_JOBS, cv=split_iterator)
     grid_search.fit(X_scaled, y)
@@ -187,8 +185,7 @@ def gradient_boosting_exp(X, y, data, split_iterator):
     gradient_boosting = sklearn.ensemble.GradientBoostingClassifier()
     hyperparameter_space = {
         "learning_rate": [0.75, 0.9, 1.],
-        "min_samples_leaf": [20],
-        # "subsample": [0.8, 0.9, 1.]
+        "min_samples_leaf": [20]
     }
 
     grid_search = sklearn.grid_search.GridSearchCV(gradient_boosting, hyperparameter_space, n_jobs=N_JOBS, cv=split_iterator,
@@ -197,7 +194,7 @@ def gradient_boosting_exp(X, y, data, split_iterator):
 
     print "Gradient boosting classifier"
     print_tuning_scores(grid_search)
-    print_feature_importances(data.drop("IsBlueWinner", axis=1).columns, grid_search)
+    print_feature_importances(data.drop("IsBlueWinner", axis=1).columns, grid_search.best_estimator_)
 
 
 def dataframe_to_ndarrays(data):
@@ -209,6 +206,7 @@ def dataframe_to_ndarrays(data):
 
 
 def merge_roles(data, team, suffix, include_log_sum=False, include_sum=True, include_min=True, include_max=True):
+    """Merge the 5 positions for the specified team and feature suffix"""
     cols = [col for col in data.columns if team in col and col.endswith(suffix)]
     assert len(cols) == 5
 
@@ -228,6 +226,7 @@ def merge_roles(data, team, suffix, include_log_sum=False, include_sum=True, inc
 
 
 def make_diff_feature(data, feature_suffix, remove_original=True):
+    """Make a Red_X - Blue_X feature"""
     data["Delta" + feature_suffix] = data["Blue" + feature_suffix] - data["Red" + feature_suffix]
 
     if remove_original:
@@ -236,38 +235,69 @@ def make_diff_feature(data, feature_suffix, remove_original=True):
         return data
 
 
+def preprocess_champions(data, include_features):
+    """Convert Blue_1_Ahri, Blue_2_Ahri, etc to Blue_Ahri"""
+    if include_features:
+        for team in ["Blue", "Red"]:
+            cols = [col for col in data.columns if team in col and "Champ" in col]
+            indicator_dfs = [pandas.get_dummies(data[col], prefix=team) for col in cols]
+            merged = reduce(lambda a, b: a.combineAdd(b), indicator_dfs[1:], indicator_dfs[0])
+            data = pandas.concat((data.drop(cols, axis=1), merged), axis=1)
+    else:
+        data = data.drop([c for c in data.columns if "Champ" in c], axis=1)
+
+    return data
+
+
+def preprocess_summoner_spells(data, include_features):
+    """Convert Blue_1_Flash (0-1), Blue_2_Flash (0-1), etc to Blue_Flash (0-5)"""
+    if include_features:
+        for team in ["Blue", "Red"]:
+            cols = [col for col in data.columns if team in col and "Spell" in col]
+            indicator_dfs = [pandas.get_dummies(data[col], prefix="{}_Summoners".format(team)) for col in cols]
+            merged = reduce(lambda a, b: a.combineAdd(b), indicator_dfs[1:], indicator_dfs[0])
+            data = pandas.concat((data.drop(cols, axis=1), merged), axis=1)
+    else:
+        data = data.drop([c for c in data.columns if "Spell" in c], axis=1)
+
+    return data
+
+def preprocess_damage_types(data, include_qcut_features):
+    """Add damage type features for each quartile. Useful for logistic regression."""
+    if include_qcut_features:
+        for col in [c for c in data.columns if "Damage" in c]:
+            data[col + "_qcut5"] = pandas.qcut(data[col], 5)
+
+    return data
+
+
+def drop_bad_features(data):
+    """Drop features that lead to overfitting and such"""
+    # MatchId would allow overfitting for no benefit
+    # GameVersion overfit
+    bad_cols = ["MatchId", "GameVersion"]
+
+    # streak features lead to slight overfitting
+    bad_cols.extend([c for c in data.columns if "Streak" in c])
+
+    # drop the per champ*summoners win rates, overfit a little bit
+    bad_cols.extend([c for c in data.columns if "champion summoners recent" in c])
+
+    return data.drop(bad_cols, axis=1)
+
+
 def preprocess_features(data, show_example=False):
     print "Before preprocessing"
     data.info()
     print "Columns: " + ", ".join(sorted(data.columns))
 
-    data = data.drop(["MatchId", "GameVersion"], axis=1)
+    data = drop_bad_features(data)
 
-    # for col in [c for c in data.columns if "Damage" in c]:
-    #     #data[col] = pandas.qcut(data[col], 5)
-    #     data[col + "_qcut5"] = pandas.qcut(data[col], 5)
+    data = preprocess_damage_types(data, False)
 
-    data = data.drop([c for c in data.columns if "Champ" in c], axis=1)
-    # convert the per-role champions into boolean indicators per side
-    # e.g., Blue_1_Ahri, Blue_2_Ahri get merged into Blue_Ahri
-    # for team in ["Blue", "Red"]:
-    #     cols = [col for col in data.columns if team in col and "Champ" in col]
-    #     indicator_dfs = [pandas.get_dummies(data[col], prefix=team) for col in cols]
-    #     merged = reduce(lambda a, b: a.combineAdd(b), indicator_dfs[1:], indicator_dfs[0])
-    #     data = pandas.concat((data.drop(cols, axis=1), merged), axis=1)
+    data = preprocess_champions(data, False)
 
-    data = data.drop([c for c in data.columns if "Spell" in c], axis=1)
-
-    # convert the per-role summoner spells into sums per side
-    # e.g., Blue_1_Spell_1, (0-1) Blue_2_Spell_2 (0-1), ... get merged into Blue_Flash (0-5)
-    # for team in ["Blue", "Red"]:
-    #     cols = [col for col in data.columns if team in col and "Spell" in col]
-    #     indicator_dfs = [pandas.get_dummies(data[col], prefix="{}_Summoners".format(team)) for col in cols]
-    #     merged = reduce(lambda a, b: a.combineAdd(b), indicator_dfs[1:], indicator_dfs[0])
-    #     data = pandas.concat((data.drop(cols, axis=1), merged), axis=1)
-
-    # drop the streak features because they lead to severe overfitting
-    data = data.drop([c for c in data.columns if "Streak" in c], axis=1)
+    data = preprocess_summoner_spells(data, False)
 
     # merge win rates and such across the team
     for team in ["Blue", "Red"]:
@@ -284,21 +314,18 @@ def preprocess_features(data, show_example=False):
         data = merge_roles(data, team, "_PlayRate(champion season)")
         data = merge_roles(data, team, "_WinRate(champion recent)")
         data = merge_roles(data, team, "_WinRate(champion version recent)")
-        data = merge_roles(data, team, "_WinRate(champion summoners recent)")
 
         data[team + "_Combined_WinRateSum_PlayedLogSum(player champion season)"] = data[team + "_WinRate(player champion season)_Sum"] * data[team + "_NumGames(player champion season)_LogSum"]
         data[team + "_Combined_WinRateSum_PlayedLogSum(cvr ps)"] = data[team + "_WinRate(champion version recent)_Sum"] * data[team + "_NumGames(player season)_LogSum"]
 
     # a few diff features
-    for feature_suffix in ["_WinRate(champion version recent)", "_WinRate(champion recent)", "_WinRate(champion season)", "_WinRate(player season)", "_PlayRate(champion season)", "_WinRate(champion summoners recent)"]:
+    for feature_suffix in ["_WinRate(champion version recent)", "_WinRate(champion recent)", "_WinRate(champion season)", "_WinRate(player season)", "_PlayRate(champion season)"]:
         data["Delta" + feature_suffix + "_Sum"] = data["Blue" + feature_suffix + "_Sum"] - data["Red" + feature_suffix + "_Sum"]
         data = data.drop(["Blue" + feature_suffix + "_Sum", "Red" + feature_suffix + "_Sum"], axis=1)
 
 
     data = make_diff_feature(data, "_Combined_WinRateSum_PlayedLogSum(player champion season)")
     data = make_diff_feature(data, "_Combined_WinRateSum_PlayedLogSum(cvr ps)")
-
-    data = data.drop([c for c in data.columns if "champion summoners recent" in c], axis=1)
 
     data = pandas.get_dummies(data)
 
@@ -342,7 +369,6 @@ def main():
 
     if args.forest:
         random_forest(X, y, data, cross_val_splits)
-
 
     if args.logistic:
         logistic_regression(X, y, data, cross_val_splits)
